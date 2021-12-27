2e44cd2f2f0f56b57d49c3c486d1842c525fe2ad
Ricardo <GeekRicardozzZ@gmail.com>
2021-12-27 21:03:39 +0800

    file-download server

---
+++ file-server.py
@@ -0,0 +1,57 @@
+#!/usr/bin python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   file_server.py
+@Time    :   2021/12/27 19:56:49
+@Author  :   Ricardo.张葆杰
+@Version :   1.0
+@Contact :   bj.zhang@tianrang-inc.com
+@Desc    :   文件下载服务器
+'''
+
+# here put the import lib
+import uvicorn
+import os
+import argparse
+from glob import glob
+from fastapi import FastAPI
+from starlette.responses import FileResponse, HTMLResponse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--static_path", type=str, required=True, help="path to static files.")
+parser.add_argument("--prefix", type=str, default="/", help="url prefix eg:\033[31m/\033[0mstatic\033[31m/\033[0m [\033[31mtwo / !!]")
+parser.add_argument("--port", type=int, default=8080, help="port")
+args = parser.parse_args()
+
+app = FastAPI()
+static_path = os.path.abspath(args.static_path)
+
+
+@app.get(args.prefix)
+def index():
+    html = get_file_list(static_path)
+    return HTMLResponse(html)
+
+
+@app.get(args.prefix + "{filename:path}")
+async def get_file(filename: str):
+    print(filename)
+    path = os.path.join(static_path, filename)
+    if not os.path.exists(path):
+        return {'success': False, 'msg': '文件不存在！'}
+    elif os.path.isdir(path):
+        return HTMLResponse(get_file_list(path))
+    response = FileResponse(path)
+    return response
+
+
+def get_file_list(path):
+    return """<ul>{}</ul>""".format("\n".join([
+        "<li><a href='/{0}'>{0}</li>".format(it.replace(static_path + "/", ""))
+        for it in glob(path + "/*")
+    ]))
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, debug=True, host="0.0.0.0", port=args.port)
+
