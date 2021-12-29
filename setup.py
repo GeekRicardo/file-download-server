5ea5565f52087b0f9c5aaff64f0a0005bea589d2
zbj.张葆杰 <bj.zhang@tianrang-inc.com>
2021-12-29 12:38:20 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="file_download_server",
+    version="1.0.0",
+    py_modules=["file_server"],
+    author="Ricardo",
+    author_email="GeekRicardozzZ@gmail.com",
+    url="",
+    description="A simple file server",
+)
