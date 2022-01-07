a7b8cfe2553f0cb614afdd82657ae6638c575c9d
GeekRicardo <GeekRicardozzZ@gmail.com>
2022-01-07 16:45:11 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name="file_download_server",
-    version="1.0.0",
+    version="1.0.2",
     py_modules=["file_server"],
     author="Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
-    url="",
+    url="https://github.com/GeekRicardo/file-download-server",
     description="A simple file server",
 )
