8f885b99b7865b80bfc9e1d648e8af1d5851f7d2
GeekRicardo <GeekRicardozzZ@gmail.com>
2022-01-07 17:54:51 +0800

    change distutils into setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="file_download_server",
     version="1.0.2",
-    py_modules=["file_server"],
+    packages=["file_server"],
     author="Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
     url="https://github.com/GeekRicardo/file-download-server",
+    install_requires=["fastapi", "uvicorn"]
     description="A simple file server",
 )
