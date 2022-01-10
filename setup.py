fd857eb1f7a23319caf261a71fd17694fbb43311
GeekRicardo <GeekRicardozzZ@gmail.com>
2022-01-10 18:02:05 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="file_download_server",
-    version="1.0.3",
-    packages=["file_server"],
+    version="1.0.4",
+    packages=["."],
     author="Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
     url="https://github.com/GeekRicardo/file-download-server",
