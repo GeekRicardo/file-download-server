e686207a9b19a49cfb2b772fc045eff17e7da501
GeekRicardo <GeekRicardozzZ@gmail.com>
2022-01-17 10:00:33 +0800

    update requirement

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ python_version = int("".join(platform.python_version().split(".")[:2]))
 
 setup(
     name="file_download_server",
-    version="1.0.8",
+    version="1.0.9",
     packages=["."],
     author="Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
     url="https://github.com/GeekRicardo/file-download-server",
-    install_requires=["fastapi", "python-multipart", "uvicorn~=" + ("0.16.0" if python_version == 36 else "0.17.0")],
+    install_requires=[
+        "fastapi",
+        "python-multipart",
+        "uvicorn~=" + ("0.16.0" if python_version == 36 else "0.17.0"),
+    ],
     description="A simple file server",
 )
