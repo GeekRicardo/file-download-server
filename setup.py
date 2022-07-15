9f599dbdefc301baf07e6ae8da5448108141d12e
Ricardo <GeekRicardozzZ@gmail.com>
2022-07-15 22:08:25 +0800

    build(pip): update requirements version

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
+import platform
+
+python_version = int("".join(platform.python_version().split(".")[:2]))
 
 setup(
     name="file_download_server",
-    version="1.0.5",
+    version="1.0.6",
     packages=["."],
     author="Ricardo",
     author_email="GeekRicardozzZ@gmail.com",
     url="https://github.com/GeekRicardo/file-download-server",
-    install_requires=["fastapi", "uvicorn"],
+    install_requires=["fastapi", "uvicorn~=" + ("0.16.0" if python_version == 36 else "0.17.0")],
     description="A simple file server",
 )
